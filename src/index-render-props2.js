0ae21234abedeb5e7101580b5d768b2d1546c4bb
杜飞虎 <980517916@qq.com>
2021-03-01 19:35:40 +0800

    hoc

---
+++ src/index-render-props2.js
@@ -0,0 +1,36 @@
+/*
+ * @Author: dfh
+ * @Date: 2021-02-24 18:18:22
+ * @LastEditors: dfh
+ * @LastEditTime: 2021-03-01 19:21:16
+ * @Modified By: dfh
+ * @FilePath: /day25-react/src/index-render-props1.js
+ */
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class MouseTracker extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { x: 0, y: 0 };
+  }
+
+  handeMouseMove = event => {
+    this.setState({
+      x: event.clientX,
+      y: event.clientY
+    })
+  }
+
+  render() {
+    return <div onMouseMove={this.handeMouseMove}>
+      {this.props.children(this.state)}
+    </div>
+  }
+}
+ReactDOM.render(<MouseTracker >{
+  props => <>
+    <h1>移动鼠标</h1>
+    <p>当前的鼠标位置是：({props.x},{props.y})</p>
+  </>
+}</MouseTracker>, document.getElementById('root'));
--- src/index.js
+++ src/index.js
@@ -2,35 +2,41 @@
  * @Author: dfh
  * @Date: 2021-02-24 18:18:22
  * @LastEditors: dfh
- * @LastEditTime: 2021-03-01 19:21:16
+ * @LastEditTime: 2021-03-01 19:35:21
  * @Modified By: dfh
- * @FilePath: /day25-react/src/index-render-props1.js
+ * @FilePath: /day25-react/src/index.js
  */
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-class MouseTracker extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { x: 0, y: 0 };
-  }
+function widthTracker(OldComponent) {
+  return class MouseTracker extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = { x: 0, y: 0 };
+    }
 
-  handeMouseMove = event => {
-    this.setState({
-      x: event.clientX,
-      y: event.clientY
-    })
-  }
+    handeMouseMove = event => {
+      this.setState({
+        x: event.clientX,
+        y: event.clientY
+      })
+    }
 
-  render() {
-    return <div onMouseMove={this.handeMouseMove}>
-      {this.props.children(this.state)}
-    </div>
+    render() {
+      return <div onMouseMove={this.handeMouseMove}>
+        <OldComponent {...this.state} />
+      </div>
+    }
   }
 }
-ReactDOM.render(<MouseTracker >{
-  props => <>
+
+function Show(props) {
+  return <>
     <h1>移动鼠标</h1>
     <p>当前的鼠标位置是：({props.x},{props.y})</p>
   </>
-}</MouseTracker>, document.getElementById('root'));
+}
+
+const MouseTracker = widthTracker(Show);
+ReactDOM.render(<MouseTracker />, document.getElementById('root'));
